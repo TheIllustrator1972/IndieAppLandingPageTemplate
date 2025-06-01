d555fe5496f043255f5bb1ed1987e734c88dd4c8
Nilesh Kamble <nileshkamble54321@gmail.com>
2025-06-01 11:55:58 +0530

    - Updated Readme

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,9 @@
 import type { NextConfig } from "next";
 
-const isGithubPages = process.env.NODE_ENV === "production";
-
 const nextConfig: NextConfig = {
-  output: "export",
-  basePath: isGithubPages ? "/IndieAppLandingPageTemplate" : "",
-  assetPrefix: isGithubPages ? "/IndieAppLandingPageTemplate/" : "",
+  // output: "export",
+  basePath: "/IndieAppLandingPageTemplate",
+  assetPrefix: "/IndieAppLandingPageTemplate/",
 };
 
 export default nextConfig;
